58e80e72dd8c95be95cdfd871b577aba9b24e4c4
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-05 18:23:43 +0900

    fix syntax error

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -23,6 +23,6 @@ class Users::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(
-      :sign_in, keys: [:email. :name, :password, :password_confirmation])
+      :sign_in, keys: [:email, :name, :password, :password_confirmation])
   end
 end
