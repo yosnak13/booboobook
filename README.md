191894032e8873d58343b0536c6190b55b7fe88a
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-05 19:14:22 +0900

    git ignore is working

---
+++ README.md
@@ -0,0 +1,7 @@
+# 開発環境
+- Ruby:2.6.5
+- rails:5.2.6
+- db:mysql 5.7
+- docker version:20.10.7
+ref: https://zenn.dev/tmasuyama1114/articles/rails-docker-5x-how-to
+Hello world
---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module App
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+  end
+end
