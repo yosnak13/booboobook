6c0e20d756d70e076ded38c3dda614b940e40d20
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-06 20:17:35 +0900

    開発環境のメール設定を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,22 +59,21 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  Rails.application.configure do
-    # default url
-    config.action_mailer.default_url_options = {
-      protocol: 'https',
-      host: ENV["APP_ROOT_URL"]
-    }
-    # mail setting
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :user_name => ENV["GMAIL_ADDRESS"],
-      :password => ENV["GMAIL_2FACTOR_PASSWORD"],
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
-  end
+
+  # default url
+  config.action_mailer.default_url_options = {
+    protocol: 'https',
+    host: ENV["ROOT_URL"]
+  }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV["GMAIL_ADDRESS"],
+    :password => ENV["GMAIL_2FACTOR_PASSWORD"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
