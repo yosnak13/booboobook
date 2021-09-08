da31da543c3fb89ac49b6ab407878a92e649c664
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-08 21:01:54 +0900

    seedで豚さんデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pork.create!(
+  [
+    {
+      name: 'ランドレース',
+      description: '大型で、毛色は白。日本の豚肉の主要な品種。',
+      evolution_level: 20,
+      photo: File.open('./app/assets/images/test1.jpg')
+    },
+    {
+      name: '大ヨークシャー',
+      description: '発育も早く、純粋種としてはランドレース種についで多数飼育されています。',
+      evolution_level: 30,
+      photo: File.open('./app/assets/images/test2.jpg')
+    },
+    {
+      name: 'ハンプシャー',
+      description: '性質は活発、採食性が強いので放飼に適しています。体質は強健ですが、暑さにやや弱いです。',
+      evolution_level: 25,
+      photo: File.open('./app/assets/images/test3.jpg')
+    }
+  ]
+)
