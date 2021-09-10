d13a3e33d04434f4ff0bcba3c94034a7ca5ac807
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-10 20:35:44 +0900

    進化を考慮した豚データをseedで投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,19 +11,43 @@ Pork.create!(
       name: 'ランドレース',
       description: '大型で、毛色は白。日本の豚肉の主要な品種。',
       evolution_level: 20,
+      evolve_into: '',
       photo: File.open('./app/assets/images/test1.jpg')
     },
     {
-      name: '大ヨークシャー',
-      description: '発育も早く、純粋種としてはランドレース種についで多数飼育されています。',
+      name: '中ヨークシャー',
+      description: '中型で、全体におおむね長方形。顔は短く、顔面はしゃくれている。色は白。飼養頭数が少なくなっているが、肉質の優れた品種である。',
       evolution_level: 30,
+      evolve_into: '大ヨークシャー',
       photo: File.open('./app/assets/images/test2.jpg')
+    },
+    {
+      name: '大ヨークシャー',
+      description: '大型で、体積に富み、全体におおむね長方形。顔は長めで、顔面はわずかにしゃくれている。色は白。主要な雌系品種として飼養頭数も多い。',
+      evolution_level: '',
+      evolve_into: '',
+      photo: File.open('./app/assets/images/test3.jpg')
     },
     {
       name: 'ハンプシャー',
       description: '性質は活発、採食性が強いので放飼に適しています。体質は強健ですが、暑さにやや弱いです。',
       evolution_level: 25,
-      photo: File.open('./app/assets/images/test3.jpg')
+      evolve_into: '',
+      photo: File.open('./app/assets/images/test1.jpg')
+    },
+    {
+      name: 'バークシャー',
+      description: '中型で、全体におおむね長方形。顔は長めで、顔面はわずかにしゃくれている。色は黒で、眉間、四肢端及び尾端が白。いわゆる黒豚として肉質の良さが特徴。',
+      evolution_level: 25,
+      evolve_into: 'デュロック',
+      photo: File.open('./app/assets/images/test4.jpg')
+    },
+    {
+      name: 'デュロック',
+      description: '大型に近く、全体におおむね弓状。顔は長めで、顔面はわずかにしゃくれ、色は褐色。主要な雄系品種として飼養頭数も多い。',
+      evolution_level: '',
+      evolve_into: '',
+      photo: File.open('./app/assets/images/test5.jpg')
     }
   ]
 )
