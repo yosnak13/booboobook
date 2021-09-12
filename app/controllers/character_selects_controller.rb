95e97e480f0bda8d6eb2fd4ff68279b503bfb9d3
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-12 23:16:15 +0900

    初回キャラのcreateアクションを定義。（save後のリダイレクト先の引数は変更可能性あり）

--- app/controllers/character_selects_controller.rb
+++ app/controllers/character_selects_controller.rb
@@ -2,21 +2,25 @@ class CharacterSelectsController < ApplicationController
 
   def new
     @character = Character.new
+    @pork = Pork.find(1)
   end
 
   def create
-    @character = current_user.Character.create(character_params)
+    @character = current_user.characters.build(character_params)
+    @character.user_id = current_user.id
+    binding.pry
     if @character.save
-      redirect_to root_path
+      flash.now[:notice] = "読書をして#{@character.name}を育ててみましょう"
+      redirect_to character_path(@character)
     else
-      render root_path
+      render :new
     end
   end
 
   private
 
   def character_params
-    params.require(:characters).permit(
-      :name, :character_type, :level, :exp, :description, :photo)
+    params.require(:character).permit(
+      :name, :character_type, :level, :exp, :description, :photo, :user_id)
   end
 end
