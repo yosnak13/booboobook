73f1ff03d3096dde4b1e1e2c372fb61adcc019f3
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-16 23:01:43 +0900

    不要メソッドをコメントアウト

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,17 +6,17 @@ class CharactersController < ApplicationController
   end
 
   def new
-    @characters = Character.new
+    # @characters = Character.new
   end
 
   def create
-    @pork.create_users_pork
-    @character = current_user.Character.create(character_params)
-    if @character.save
-      redirect_to root_path
-    else
-      render root_path
-    end
+    # @pork.create_users_pork
+    # @character = current_user.Character.create(character_params)
+    # if @character.save
+    #   redirect_to root_path
+    # else
+    #   render root_path
+    # end
   end
 
   def show
