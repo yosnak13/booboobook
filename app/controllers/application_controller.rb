c0e22c635219969d132f7aaf3acb522b4c0721dc
1994reka3 <rik.s.1005@gmail.com>
2021-09-21 00:09:22 +0900

    ログイン・ログアウト後のパス指定（ユーザー、管理者それぞれ）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    if current_user
+      user_path(current_user)
+    else
+      admins_users_index_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    if current_user
+      new_user_session_path
+    else
+      new_admin_session_path
+    end
+  end
 end
