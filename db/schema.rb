a4522b9f06173f722f3926270f18abc153c053e3
“Toshifumi <oooootani.1145@gmail.com>
2021-10-03 23:28:24 +0900

    css作成

--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2021_09_20_045439) do
     t.integer "total_read_time"
     t.integer "status"
     t.string "memo"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
