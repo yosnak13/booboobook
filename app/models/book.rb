77535ffefd130d52b3ea4e219cf11813f2fcd0f7
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-10-05 01:52:52 +0900

    study_timesを介した多対他を実現

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :study_time
+  has_many :characters, through: :study_times
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ApplicationRecord
   belongs_to :user
   has_many :study_time
+  has_many :books, through: :study_times
 end
