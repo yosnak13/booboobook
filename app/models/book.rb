b47f9bd7b66c9345d1a24d73cd0394f383bdbc49
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-10-08 20:19:51 +0900

    StudyTimeとのリレーションを見直し

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :study_time
-  has_many :characters, through: :study_times
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,3 @@
 class Character < ApplicationRecord
   belongs_to :user
-  has_many :study_time
-  has_many :books, through: :study_times
 end
