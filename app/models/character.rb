295cb02cf293011d14a3089f864341f8ec10c24e
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-10-09 11:27:27 +0900

    経験値がnilになってしまう不具合を修正

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
 class Character < ApplicationRecord
   belongs_to :user
+
+  with_options presence:true do
+    validates :name, format: {with:/\A[ァ-ヶー－]+\z/}
+    validates :character_type
+    validates :level, numericality: true
+    validates :exp, numericality: true
+    validates :description, format: {with:/\A[ぁ-んァ-ン一-龥]/}
+  end
+
+  def add_exp
+    @current_exp = @character.exp
+    @character.increment(:exp, @current_exp)
+    @character.save!
+  end
 end
