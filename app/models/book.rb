ed1bb0e19bb8db70a57f8edf2cd1c6a6cf0fefad
1994reka3 <rik.s.1005@gmail.com>
2021-10-25 02:28:27 +0900

    total_read_timeを数値からTime型に表示するメソッド作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,9 @@ class Book < ApplicationRecord
   validates :book_name, presence: true
   validates :status, presence: true
   enum status:{ 未読: 0, 読書中: 1, 読了: 2 }
+
+  def total_read_time_sec #秒数換算してTimeオブジェクトを返すメソッド
+    self.total_read_time *= 60
+    self.total_read_time = Time.at(total_read_time)
+  end
 end
