cdaf62a08d36c741a39681152ae4f1ed6195ddb2
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-11-24 23:35:33 +0900

    暫定メソッド

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -50,15 +50,24 @@ class BooksController < ApplicationController
   end
 
   def select_book
-    @book = Book.find(params[:id])
-    @books = current_user.books.where(status: 0)or(books.where(status: 2))or(books.where(status: 3))
+    @book = current_user.books.find_by(status: 1)
+    @books = current_user.books.where(status: 0).or(current_user.books.where(status: 2))
   end
 
   def change_book
-    if @books.update(change_book_status_params)
+    #postされた書籍を拾う
+    @change_book_status = Book.find(params[:id])
+    #読書中の書籍を拾う
+    @edit_status_book = current_user.books.find_by(status: 1)
+    binding.pry
+    if @change_book_status.update(change_book_status_params)
+      @change_book_status.save
+      @edit_status_book.update(status: 2)
+      @edit_status_book.save
       flash[:notice] = "読書する書籍を変更しました"
       redirect_to study_times_user_path(current_user)
     else
+      flash[:alart] = "書籍を変更できませんでした"
       render :select_book
     end
   end
@@ -75,10 +84,5 @@ class BooksController < ApplicationController
 
   def change_book_status_params
     params.require(:book).permit(:status)
-  end
-
-  def find_book
-    @user = current_user
-    @book = @user.books
   end
 end
