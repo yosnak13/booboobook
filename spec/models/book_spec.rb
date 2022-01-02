ad2f1880690de28c715c2872e2cf4a597704d71f
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-02 12:17:53 +0900

    名前がない書籍は登録できないテスト

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Book, type: :model do
         new_book = FactoryBot.build(:test_book)
         expect(new_book).to be_valid
       end
+
+      it "正しく登録できないとき" do
+        error_book = FactoryBot.build(:test_book, book_name: "")
+        expect(error_book).not_to be_valid
+      end
     end
   end
 end
