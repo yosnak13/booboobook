eb659b4bc9af2dc4a2dfc06d57137411be984399
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-02 12:07:34 +0900

    userモデルへのassosiationのあるBookのFactoryBotを作成

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :book do
-    
+  end
+
+  factory :test_book, class: 'book' do
+    association :user, factory: :test_user_tanaka
+    isbn { "0000000000" }
+    book_name { "プログラムの本 その1" }
+    total_read_time { 0 }
+    status { 0 }
+    memo { "最初に登録して1時間よみます" }
   end
 end
