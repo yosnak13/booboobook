fadc3f4895ac916a4ed6d6c1bc7a21ebad2f4f33
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-02 12:26:10 +0900

    正常登録パターンを追加

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,12 +5,15 @@ RSpec.describe Book, type: :model do
     context "書籍を新たに登録するとき" do
       it "正しく登録できるとき" do
         new_book = FactoryBot.build(:test_book)
+        # isbn不要でも登録可能
+        isbn_blank_book = FactoryBot.build(:test_book, isbn: "")
         expect(new_book).to be_valid
+        expect(isbn_blank_book).to be_valid
       end
 
       it "正しく登録できないとき" do
-        error_book = FactoryBot.build(:test_book, book_name: "")
-        expect(error_book).not_to be_valid
+        name_blank_book = FactoryBot.build(:test_book, book_name: "")
+        expect(name_blank_book).not_to be_valid
       end
     end
   end
