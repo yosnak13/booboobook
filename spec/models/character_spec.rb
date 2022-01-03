14bfa552d197cdbe86c16cca4780707b06fc0b22
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-03 14:22:35 +0900

    画像アップロードテストの実装（ActiveStorage::IntegrityError）

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Character, type: :model do
   context "初回登録にてキャラクターを登録するとき" do
+    before do
+      @character = FactoryBot.build(:character)
+      @character.photo = fixture_file_upload("/images/TestLandrace.png")
+    end
+
     it "正しく登録できるとき" do
-      new_character = FactoryBot.create(:character)
-      expect(new_character).to be_valid
+      expect(@character).to be_valid
     end
   end
 end
