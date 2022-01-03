ad84715f0317c11d48bae4169c268983f9cf67e6
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-03 10:58:35 +0900

    study_timesのテスト開発を保留（Characterテスト以降再開）

--- spec/factories/study_times.rb
+++ spec/factories/study_times.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :study_time do
-    
   end
+
+  # factory :study_time, class: "study_time" do
+  #   association :book, factory: :test_book
+  #   user { book.user }
+  #   study_date { 30 }
+  #   study_time {  }
+  # end
 end
