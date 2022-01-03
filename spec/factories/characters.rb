067a9c844ec531c80a1d3059374ce17447ad9d32
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-03 11:29:02 +0900

    キャラクター登録のバリデーションテストの実装（ActiveStrageを一旦無視）

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :character do
-    
+    association :user, factory: :test_user_tanaka
+    name { "ランドレース" }
+    character_type { 0 }
+    level { 1 }
+    exp { 0 }
+    description { "大型で、毛色は白。日本の豚肉の主要な品種。" }
+    photo { File.open('./db/fixtures/Landrace.png') }
   end
 end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Character, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "初回登録にてキャラクターを登録するとき" do
+    it "正しく登録できるとき" do
+      new_character = FactoryBot.create(:character)
+      expect(new_character).to be_valid
+    end
+  end
 end
