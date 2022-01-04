e9da4710e190aaeff7b918f9d7c0f17dc8db780d
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-04 10:40:14 +0900

    saveテストと削除のテストを追記

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,12 +8,15 @@ RSpec.describe Book, type: :model do
         # isbn不要でも登録可能
         isbn_blank_book = FactoryBot.build(:test_book, isbn: "")
         expect(new_book).to be_valid
+        expect(new_book.save).to be_truthy
         expect(isbn_blank_book).to be_valid
+        expect(isbn_blank_book.save).to be_truthy
       end
 
       it "正しく登録できないとき" do
         name_blank_book = FactoryBot.build(:test_book, book_name: "")
         expect(name_blank_book).not_to be_valid
+        expect(name_blank_book.save).to be_falsey
       end
     end
   end
@@ -23,6 +26,12 @@ RSpec.describe Book, type: :model do
       edit_book = FactoryBot.build(:test_book)
       edit_book.update(book_name: "Ruby on Rails その1", memo: "読書完了まであと少し")
       expect(edit_book.save).to be_truthy
+    end
+
+    it "削除するとき" do
+      delete_book = FactoryBot.build(:test_book)
+      delete_book.delete
+      expect(delete_book).to be_truthy
     end
   end
 end
