581d14bf277fcfcab5012a236228761d360c0006
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-10 11:07:06 +0900

    サインアップが成功するテスト

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,24 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "ログイン" do
+  let(:user) { create(:user) }
+  let(:user_params) { attributes_for(:user) }
+  let(:invalid_user_params) { attributes_for(:user, name: "") }
+
+  describe "サインアップ" do
     before do
-      @user = create(:test_user_tanaka)
-      @character = create(:character)
+      ActionMailer::Base.deliveries.clear
     end
 
-    context "ログインするとき" do
-      it 'ログイン後#indexへページ遷移すること' do
-        sign_in @user
-        get users_path
-        expect(response).to have_http_status(200)
+    context "入力値が正しいとき" do
+      it 'リクエストが成功すること' do
+        post user_registration_path, params: { user: user_params }
+        expect(response.status).to eq 302
       end
 
-      it "ログインしないと#users/sign_upへリダイレクトすること" do
-        get users_path
-        expect(response).to redirect_to new_user_session_path
-        expect(response).to have_http_status(200)
+      it '認証メールが送信されること' do
+        post user_registration_path, params: { user: user_params }
+        expect(ActionMailer::Base.deliveries.size).to eq 1
+      end
+
+      it 'createが成功すること' do
+        expect do
+          post user_registration_path, params: { user: user_params }
+        end.to change(User, :count).by 1
+      end
+
+      it '認証ページへリダイレクトされること' do
+        post user_registration_path, params: { user: user_params }
+        expect(response).to redirect_to verify_path
       end
     end
+  end
+
+  # xdescribe "ログイン" do #処理を保留する
+  #   let(:user) { create(:user, :confirmed) }
+
+  #   context "ログインするとき" do
+  #     it 'ログイン後#indexへページ遷移すること' do
+  #       sign_in @user
+  #       get users_path
+  #       expect(response).to have_http_status(200)
+  #     end
+
+  #     it "ログインしないと#users/sign_upへリダイレクトすること" do
+  #       get users_path
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+
+  #    context "未認証のとき" do
+  #     let(:user) { create(:user) }
+  #     it "ログインできないとき" do
+  #       get users_path
+  #       expect(response).to redirect_to
+  #     end
+  #    end
   end
 end
