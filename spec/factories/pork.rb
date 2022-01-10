c132f5fcf9d001b8d8ecfbfa952e8d8f7c46f3cf
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-10 12:25:24 +0900

    idとアップロードファイル名と参照ディレクトリを修正

--- spec/factories/pork.rb
+++ spec/factories/pork.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :pork do
+    id { 1 }
     name { "ランドレース" }
     description { "大型で、毛色は白。日本の豚肉の主要な品種。" }
     evolution_level { 20 }
     evolve_into { "中ヨークシャー" }
-    photo { File.open('./db/fixtures/Landrace.png') }
+    photo { ActiveStorage::Blob.create_after_upload!(
+      io: File.open('./spec/fixtures/images/TestLandrace.png'), filename: 'TestLandrace.png'
+      )
+    }
   end
 end
