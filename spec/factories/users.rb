0bd98aa6b89ab5ba3c039d36b37ffc985387d226
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-10 11:06:52 +0900

    認証済みユーザーのFactoryBot作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
+    sequence(:email) { |n| "sample#{n}@example.com" }
+    name { "認証済みユーザー" }
+    password { "password" }
+    encrypted_password { "password" }
+
+    trait :confirmed do
+      after_create { |user| user.confirm! }
+    end
   end
 
   factory :test_user_tanaka, class: 'user' do
@@ -7,5 +15,6 @@ FactoryBot.define do
     email { "taro.tanaka@example.com" }
     password { "Adf567" }
     encrypted_password { "Adf567" }
+    confirmed_at { Date.today }
   end
 end
