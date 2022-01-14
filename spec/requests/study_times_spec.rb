76f667026bb0bfe247a6f7a64e492b6873a51a97
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-14 18:40:28 +0900

    study_times_controllerテスト用雛形作成

--- spec/requests/study_times_spec.rb
+++ spec/requests/study_times_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "StudyTimes", type: :request do
-  describe "GET /study_times" do
-    it "works! (now write some real specs)" do
-      get study_times_path
-      expect(response).to have_http_status(200)
+  describe "学習時間を投稿するとき" do
+    let(:user) { create(:user) }
+    let(:study_time) { create(:study_time)}
+
+    context "入力値が正しいとき" do
+      it "投稿できるとき" do
+        post study_times_user_path(user), params:  study_time
+        expect(response).to have_http_status(200)
+      end
     end
   end
 end
