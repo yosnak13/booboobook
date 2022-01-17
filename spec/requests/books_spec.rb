67cb15f5861f7146e4f61933a3f79b56afdff444
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-18 07:09:30 +0900

    新規書籍登録のテスト

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
-  describe "GET /books" do
-    it "works! (now write some real specs)" do
-      get books_path
-      expect(response).to have_http_status(200)
+  let(:user) { create(:user) }
+
+  describe "書籍登録" do
+    context "入力値がただしいとき" do
+      before do
+        book = build(:book)
+      end
+
+      it "書籍の登録が成功すること" do
+        get user_books_path(user)
+        post user_books_path(user), params: { new_book: FactoryBot.attributes_for(:test_book) } #修正予定
+        follow_redirect!
+        expect(response.status).to eq 200
+      end
     end
   end
 end
