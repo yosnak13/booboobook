29d2f6fd2ea48518e035d6fc9dd055e7549d91cb
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-20 07:05:35 +0900

    editテスト編集中

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Books", type: :request do
   let(:user) { create(:user) }
 
   describe "書籍登録" do
-    context "入力値がただしいとき" do
+    context "入力値が正しいとき" do
       before do
         book = build(:book)
       end
@@ -14,6 +14,18 @@ RSpec.describe "Books", type: :request do
         post user_books_path(user), params: { new_book: FactoryBot.attributes_for(:test_book) } #修正予定
         follow_redirect!
         expect(response.status).to eq 200
+      end
+
+      it "書籍の編集が成功すること" do
+        #  ActionController::UrlGenerationError: No route matches {:action=>"edit", :controller=>"books"}, missing required keys: [:id, :user_id]
+        get edit_user_book_path, params: { new_book: FactoryBot.attributes_for(:test_book) }
+        
+        patch user_book_path(user), params: { new_book: FactoryBot.attributes_for(:test_book, name: "プログラミング上級") }
+        follow_redirect!
+        expect(response.status).to eq 200
+      end
+
+      it "書籍の削除が成功すること" do
       end
     end
   end
