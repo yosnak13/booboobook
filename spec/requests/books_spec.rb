a052e9d5bd7c09a965eaa70b37b66a863f80f5fb
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-01-22 10:09:53 +0900

    let位置を移動、失敗するテストを実装

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
   let(:user) { create(:user) }
+  let(:book) { create(:test_book) }
 
   describe "書籍登録" do
     context "入力値が正しいとき" do
-      let(:book) { create(:test_book) }
-
       it "書籍の登録が成功すること" do
         get user_books_path(user)
         post user_books_path(user), params: { new_book: book }
@@ -27,6 +26,26 @@ RSpec.describe "Books", type: :request do
         delete user_book_path(user, book)
         follow_redirect!
         expect(response.status).to eq 200
+      end
+    end
+
+    context "入力値が正しくないとき" do
+      let(:invalid_book_params) { attributes_for(:book, name: "") }
+
+      it "書籍登録が失敗すること" do
+        get user_books_path(user)
+        post user_books_path(user), params: { invalid_book_params: invalid_book_params }
+        redirect_to user_books_path(user)
+        expect(response.status).to eq 302
+      end
+
+      it "書籍編集が失敗すること" do
+        get edit_user_book_path(user, book)
+        patch user_book_path(user, book), params: {
+          edit_book: invalid_book_params, book_name: ""
+        }
+        redirect_to edit_user_book_path(user, book)
+        expect(response.status).to eq 302
       end
     end
   end
