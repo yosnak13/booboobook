3b486e28c6367385c9eae59ffb951b25ca7ba5f4
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-05-05 03:17:26 +0900

    テスト

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = {
-    host: ENV["APP_ROOT_URL"],
+    host: "localhost:3000"
   }
 
   # mail setting
