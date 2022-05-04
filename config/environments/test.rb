c11010b88bf0a126022b4c9718c8fed5295c2cf9
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-05-05 02:59:23 +0900

    default_url_option

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,8 +31,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
 
-  config.action_mailer.default_url_options = { host: 'example.com'}
-
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
@@ -47,7 +45,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = {
     host: ENV["APP_ROOT_URL"],
-    port: 3000
   }
 
   # mail setting
