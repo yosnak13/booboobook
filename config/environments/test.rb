e67f8b51efffe2ab812f8b7d8435698b167caedf
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-05-06 11:27:17 +0900

    action_mailer.delivery_method = :testに設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,7 +49,6 @@ Rails.application.configure do
 
   # mail setting
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
