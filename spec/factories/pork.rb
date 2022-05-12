110eb6bbbf69d3d5243b02a2af959eb727ff07d2
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-05-12 22:00:08 +0900

    seedを使用する場合、factorybotにidが不要なため削除

--- spec/factories/pork.rb
+++ spec/factories/pork.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :pork do
-    id { 1 }
-    name { "ランドレース" }
-    description { "大型で、毛色は白。日本の豚肉の主要な品種。" }
+    name { "テストランドレース" }
+    description { "テスト用テキスト：大型で、毛色は白。日本の豚肉の主要な品種。" }
     evolution_level { 20 }
-    evolve_into { "中ヨークシャー" }
+    evolve_into { "テスト中ヨークシャー" }
     photo { ActiveStorage::Blob.create_after_upload!(
       io: File.open('./spec/fixtures/images/TestLandrace.png'), filename: 'TestLandrace.png'
       )
