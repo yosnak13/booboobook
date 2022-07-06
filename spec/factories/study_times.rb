abe365f242694e4e3544cd5f9b4610a4666a2743
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-06 11:04:44 +0900

    失敗するテスト

--- spec/factories/study_times.rb
+++ spec/factories/study_times.rb
@@ -1,11 +1,13 @@
 FactoryBot.define do
   factory :study_time do
+    association :user, factory: :test_user_tanaka
     association :book, factory: :test_book
     study_date { '2022-01-04 01:30:09' }
     study_time { 30 }
   end
 
   factory :two_hour_study, class: 'study_time' do
+    association :user, factory: :test_user_tanaka
     association :book, factory: :test_book
     study_date { '2022-01-15 07:30:00' }
     study_time { 120 }
