f16bd32d987a3d2c18134119d945c4246ae26eb4
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-06 10:11:12 +0900

    booksモデルの追加マイグレーションファイルの削除

--- db/migrate/20211023105923_change_datatype_photo_of_characters.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypePhotoOfCharacters < ActiveRecord::Migration[5.2]
-  def up
-    change_column :characters, :photo, :string
-  end
-end
--- db/migrate/20211024173351_change_total_read_time_of_books.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTotalReadTimeOfBooks < ActiveRecord::Migration[5.2]
-  def up
-    change_column :books, :total_read_time, :integer, default: 0, null: false
-  end
-
-  def down
-    change_column :books, :total_read_time, :integer
-  end
-end
