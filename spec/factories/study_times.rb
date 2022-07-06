6ff4614cd65bcaa9706c48fb1fd84ec8c83a5c17
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-07 07:52:21 +0900

    user_idが重複しないよう、bookのuser_idを呼び出す

--- spec/factories/study_times.rb
+++ spec/factories/study_times.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :study_time do
-    association :user, factory: :test_user_tanaka
     association :book, factory: :test_book
+    user { book.user }
     study_date { '2022-01-04 01:30:09' }
     study_time { 30 }
   end
 
   factory :two_hour_study, class: 'study_time' do
-    association :user, factory: :test_user_tanaka
     association :book, factory: :test_book
+    # user { book.user }
     study_date { '2022-01-15 07:30:00' }
     study_time { 120 }
   end
