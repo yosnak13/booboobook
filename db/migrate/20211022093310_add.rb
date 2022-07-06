0d604eaf278c75bbd6238d270925c47397e81dcb
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-06 10:16:14 +0900

    マイグレーションファイル名を修正

--- db/migrate/20211022093310_add.rb
+++
@@ -1,5 +0,0 @@
-class Add < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :is_deleted, :boolean, default: false, null: false
-  end
-end
--- db/migrate/20211022093310_add_column_of_users.rb
+++ db/migrate/20211022093310_add_column_of_users.rb
@@ -1,6 +1,5 @@
 class Add < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :is_deleted, :boolean, default: false, null: false
-    add_column :users, :references, foreign_key: true
   end
 end
