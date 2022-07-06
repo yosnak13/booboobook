6d065a188595b3c1990ddd31647728b047aafccc
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-06 10:43:29 +0900

    Userモデルへのリレーションを追加

--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -1,5 +1,6 @@
 class StudyTime < ApplicationRecord
   belongs_to :book
+  belongs_to :user
 
   with_options presence: true do
     validates :study_time
@@ -10,6 +11,7 @@ class StudyTime < ApplicationRecord
     study_time.study_time = study_time_params[:study_time]
     study_time.study_date = study_time_params[:study_date]
     study_time.book_id = study_time_params[:book_id]
+    study_time.user_id = study_time_params[:user_id]
     save
   end
 end
