0ea056751ab54925172a83fbcf0d19417d65e7c1
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-06 10:22:05 +0900

    ファイル名とクラス名を修正

--- db/migrate/20211022093310_add_column_to_users.rb
+++ db/migrate/20211022093310_add_column_to_users.rb
@@ -1,4 +1,4 @@
-class Add < ActiveRecord::Migration[5.2]
+class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :is_deleted, :boolean, default: false, null: false
   end
