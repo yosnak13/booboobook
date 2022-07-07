6e8c8ef2b44bfd7b318b5a84cf2e2b68bab354ce
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-07 09:26:19 +0900

    UserとStudyTimeをアソシエーションしたため、学習時間記録メソッドを簡素化

--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -6,12 +6,4 @@ class StudyTime < ApplicationRecord
     validates :study_time
     validates :study_date
   end
-
-  def save_study_time(study_time, study_time_params)
-    study_time.study_time = study_time_params[:study_time]
-    study_time.study_date = study_time_params[:study_date]
-    study_time.book_id = study_time_params[:book_id]
-    study_time.user_id = study_time_params[:user_id]
-    save
-  end
 end
