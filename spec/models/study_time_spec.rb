93ce138985eac2ed664a2f931fa417e0d37bfc54
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-09 10:23:44 +0900

    暫定のバリデーションテスト

--- spec/models/study_time_spec.rb
+++ spec/models/study_time_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe StudyTime, type: :model do
   describe "読書時間を登録するとき" do
-    let(:post_study_time) { create(:study_time) }
+    let(:post_study_time) { create(:normal_study_time) }
+
     it "入力値が正しいとき" do
       expect(post_study_time).to be_valid
       expect(post_study_time.save).to be_truthy
     end
 
     it "入力値が誤りの場合" do
-      wrong_study_time = build(:study_time, study_time: "")
+      wrong_study_time = build(:normal_study_time, study_time: "")
       expect(wrong_study_time).not_to be_valid
       expect(wrong_study_time.save).to be_falsey
     end
