2144a154386ad918a1f5afe941fdba6eed910574
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-10 06:41:11 +0900

    通らないテストだが、暫定で可とする

--- spec/factories/study_times.rb
+++ spec/factories/study_times.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :study_time do
+    association :user, factory: :user
     association :book, factory: :test_book
-    user { book.user }
     study_date { '2022-01-04 01:30:00' }
     study_time { 30 }
     created_at { '2022-01-04 02:00:00' }
--- spec/models/study_time_spec.rb
+++ spec/models/study_time_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe StudyTime, type: :model do
   describe "読書時間を登録するとき" do
-    let(:post_study_time) { create(:study_time) }
+    # let(:post_study_time) { create(:study_time) }
 
     it "入力値が正しいとき" do
+      post_study_time = build(:study_time)
       expect(post_study_time).to be_valid
       expect(post_study_time.save).to be_truthy
     end
