cb4274feb13a9809371634597c355284891be7c5
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-09 09:29:33 +0900

    user_idとbook_idを必須属性として追加

--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -3,6 +3,8 @@ class StudyTime < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
+    validates :user_id
+    validates :book_id
     validates :study_time
     validates :study_date
   end
