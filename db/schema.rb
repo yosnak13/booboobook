708a4e70ab6c06583548487cd7f16bc49444c52d
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-07-18 22:26:30 +0900

    study_dateのデータ型をdateに修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_112526) do
+ActiveRecord::Schema.define(version: 2022_07_18_132324) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
